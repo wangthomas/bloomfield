e8debdae2d6dce1cdc624aa974bf3c764fd0eb79
giraffewt <bitwangtuo@gmail.com>
2018-04-26 15:33:11 -0700

    add_benchmark

--- plainFilter_test.go
+++ plainFilter_test.go
@@ -3,31 +3,47 @@ package plainFilter
 import (
 	"testing"
 	"hash/fnv"
+	"math/rand"
 	"github.com/OneOfOne/xxhash"
 	. "gopkg.in/check.v1"
 )
+
+const (
+	letterBytes		= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$^&*()_+-="
+	capacity 		= 10000
+	probability		= 0.0001
+	key_length		= 50
+	num_keys		= 1000
+) 
 
 
 func Test(t *testing.T) { TestingT(t) }
 
 type BloomSuite struct {
-	filter *PlainFilter
+	filter 	*PlainFilter
+	keys	[]string
 }
 
 var _ = Suite(&BloomSuite{})
 
 func (s *BloomSuite) SetUpTest(c *C) {
-	cap := uint64(10000)
-	s.filter = NewPlainFilter(cap, 0.0001)
-	Add("test", s.filter)
-	Add("test_company_0x12446755073609551590", s.filter)
+	cap := uint64(capacity)
+	s.filter = NewPlainFilter(cap, probability)
+	// Init s.keys with random strings
+	for i := 0; i < num_keys; i++ {
+		b := make([]byte, key_length)
+		for j := range b {
+			b[j] = letterBytes[rand.Intn(len(letterBytes))]
+		}
+		s.keys = append(s.keys, string(b))
+	}
 }
 
 
 func (s *BloomSuite) TestNew(c *C) {
 
-	c.Assert(s.filter.probability, Equals, 0.0001)
-	c.Assert(s.filter.capacity, Equals, uint64(10000))
+	c.Assert(s.filter.probability, Equals, probability)
+	c.Assert(s.filter.capacity, Equals, uint64(capacity))
 	c.Assert(s.filter.num_hash, Equals, uint64(14))
 	c.Assert(s.filter.num_bits_inslice, Equals, uint64(13693))
 	c.Assert(len(s.filter.bitmap), Equals, 23963)
@@ -35,14 +51,43 @@ func (s *BloomSuite) TestNew(c *C) {
 
 
 func (s *BloomSuite) TestHas(c *C) {	
-	c.Assert(Has("test", s.filter), Equals, true)
-	c.Assert(Has("test_company_0x12446755073609551590", s.filter), Equals, true)
+	for _, key := range s.keys {
+		Add(key, s.filter)
+	}
+
+	for _, key := range s.keys {
+		c.Assert(Has(key, s.filter), Equals, true)
+	}
 }
 
 
+// % is not in the letterBytes. With reasonably small false positive rate we should not fail this test case.
+// But take it as a guarantee
 func (s *BloomSuite) TestNotHas(c *C) {
-	c.Assert(Has("new_test", s.filter), Equals, false)
-	c.Assert(Has("test_company_0x12446755073609551591", s.filter), Equals, false)
+
+	for _, key := range s.keys {
+		Add(key, s.filter)
+	}
+
+	c.Assert(Has("2CTM26WgMtQTcUyjbRuucAM6Th2j4nHVYbzsAy1uVBOlFAuEs%", s.filter), Equals, false)
+	c.Assert(Has("6sFgnz0KPRy0dDer7hfLUhE6QJNVUVXZfY%fvfQ9hSu29MpDuU", s.filter), Equals, false)
+	c.Assert(Has("juXWihBRrliVwXkB9Ak9%nManCN72ia50paT7fV1fkcx9EcbP5", s.filter), Equals, false)
+	c.Assert(Has("7DdxUYlDyhGxm%tBX1G4ELk0RekTboc2PKo3QGLTYEXaDwYoXg", s.filter), Equals, false)
+	c.Assert(Has("%eaVQoClbdsC07SjG0j991KPWaPHOSw8FgWJfp7PEjFjcZA3Bt", s.filter), Equals, false)
+}
+
+
+func (s *BloomSuite) BenchmarkAdd(c *C) {
+	for n := 0; n < c.N; n++ {
+		Add(s.keys[0], s.filter)
+	}
+}
+
+
+func (s *BloomSuite) BenchmarkHas(c *C) {
+	for n := 0; n < c.N; n++ {
+		Has(s.keys[0], s.filter)
+	}
 }
 
 
