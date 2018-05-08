0c35e2a14eb3192fefb7be8fe9772b2c460e0f5b
giraffewt <bitwangtuo@gmail.com>
2018-05-07 23:05:14 -0700

    add_gRPC_generator_guide

---
+++ interfaces/gRPC/bloomfieldpb.go
@@ -0,0 +1,9 @@
+package bloomfieldpb
+
+//MacOS gRPC genetator guide 
+//brew install protobuf
+//go get -u google.golang.org/grpc
+//go get -u github.com/golang/protobuf/protoc-gen-go
+//export PATH=$PATH:$GOPATH/bin
+//cd bloomfield/interfaces/
+//protoc -I gRPC/ gRPC/bloomfieldpb.proto --go_out=plugins=grpc:gRPC
