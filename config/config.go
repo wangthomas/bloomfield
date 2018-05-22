d26d21707f97786042df95345f69c382178d292b
giraffewt <bitwangtuo@gmail.com>
2018-05-21 22:57:07 -0700

    add config support

---
+++ config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+)
+
+var Config Configuration
+
+type Configuration struct {
+	Port       string 
+}
+
+
+
+func LoadConfig(configFile string) {
+
+	if _, err := toml.DecodeFile(configFile, &Config); err != nil {
+		fmt.Println("Cannot load config file. Using default config. Err is %s", err)
+		return
+	}
+}
+
+func LoadDefault() {
+	Config.Port = "8679"
+}
+
